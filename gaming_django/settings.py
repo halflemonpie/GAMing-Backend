633f25ac621e1ac7d3821b05caf7492142350fd2
Yonghai <dylanyonghaih@gmail.com>
2022-10-05 19:00:32 -0400

    user model working

--- gaming_django/settings.py
+++ gaming_django/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'core',
+    'rest_framework.authtoken',
     'drf_spectacular',
+    'core',
+    'user',
 ]
 
 REST_FRAMEWORK = {
