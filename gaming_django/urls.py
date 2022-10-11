2c82024c0ee92c8872da2ea7353d0dc33f2126f5
Yonghai <dylanyonghaih@gmail.com>
2022-10-10 22:11:57 -0400

    change url setting for deployment

--- gaming_django/urls.py
+++ gaming_django/urls.py
@@ -32,8 +32,8 @@ urlpatterns = [
     path('api/profile/', include('userprofile.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
+# if settings.DEBUG:
+#     urlpatterns += static(
+#         settings.MEDIA_URL,
+#         document_root=settings.MEDIA_ROOT,
+#     )
